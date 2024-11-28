ce07f1821a86f7778bec479362ad13791da9dd3d
Sriharsha-palaparthi <pvasriharsha@gmail.com>
2024-11-29 00:26:31 +0530

    Update python_basics.py

--- Basic0/python_basics.py
+++ Basic0/python_basics.py
@@ -1,1 +1,20 @@
 #python is basic and widely used for learning Ai alongside focus on math part. 
+#python has easy to ready code.
+#python3 is latest version used in this tutorial
+#best way of learning is referring any online python practice resource and comeback here to start at an intermediary level.
+# https://www.w3schools.com/python/python_intro.asp 
+
+# a simple print statement
+print("Namaste")
+
+#assigning user input
+luckynumber = int(input("Namaste,Enter your lucky number"))
+print(luckynumber)
+
+#Best dev guys know the difference between runtime and compile time errors
+#avoid runtime errors by testing with boudaries
+
+
+
+
+
